0210427574735c0bf3e37ed1d240f73bad47daa0
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 14:30:06 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  children_of_thanos = ["Ebony Maw", "Proxima Midnight", "Corvus Glaive", "Cull Obsidian"]
+  children_of_thanos = Array.new
+end
+
+def array_with_two_elements
+  strongest_avengers = ["Thor", "Hulk"]
+  strongest_avengers
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  first_index_value = array.first
+  return first_index_value
+end
+
+def last_element_with_array_methods(array)
+  last_index_value = array.last
+  return last_index_value
+end
+
+def length_of_array(array)
+  array_length = array.length
+  return array_length
+end
