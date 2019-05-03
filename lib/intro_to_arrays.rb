f3607c2b3c2d2e747e17ce3e1536d75c8b70f93e
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-03 14:24:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,8 @@ def instantiate_new_array
   children_of_thanos = ["Ebony Maw", "Proxima Midnight", "Corvus Glaive", "Cull Obsidian"]
   children_of_thanos = Array.new
 end
+
+def array_with_two_elements
+  strongest_avengers = ["Thor", "Hulk"]
+  strongest_avengers
+end
